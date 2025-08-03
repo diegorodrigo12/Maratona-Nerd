6773969580ccd8e66151a9539c571b2c2907554b
Diego Rodrigo <tecnoplayhardware@gmail.com>
2025-08-03 11:25:20 -0300

    teste

--- salvar_comentario.php
+++ salvar_comentario.php
@@ -7,17 +7,21 @@ $data = json_decode(file_get_contents("php://input"), true);
 $nome = htmlspecialchars($data["nome"]);
 $comentario = htmlspecialchars($data["comentario"]);
 
-$conn = new mysqli("sql308.infinityfree.com", "if0_39624833", "YXO4pGTV5z5G", "if0_39624833_Maratonando");
+$conn = new mysqli("sql308.infinityfree.com", "if0_39624833", "YX04pGTV5z5G", "if0_39624833_Maratonando");
 
 if ($conn->connect_error) {
   http_response_code(500);
-  echo json_encode(["erro" => "Erro na conexão"]);
+  echo json_encode(["erro" => "Erro na conexão: " . $conn->connect_error]);
   exit;
 }
 
 $stmt = $conn->prepare("INSERT INTO comentarios (nome, comentario) VALUES (?, ?)");
 $stmt->bind_param("ss", $nome, $comentario);
-$stmt->execute();
 
-echo json_encode(["mensagem" => "Comentário salvo com sucesso."]);
+if ($stmt->execute()) {
+  echo json_encode(["mensagem" => "Comentário salvo com sucesso."]);
+} else {
+  http_response_code(500);
+  echo json_encode(["erro" => "Erro ao inserir comentário."]);
+}
 ?>
