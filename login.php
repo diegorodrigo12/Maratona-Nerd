4ee3e92a282d4bd69a499f94027c4319d0452d1e
Diego Rodrigo <tecnoplayhardware@gmail.com>
2025-08-25 17:09:35 -0300

    teste

--- login.php
+++
@@ -1,5 +0,0 @@
-<form action="login_action.php" method="POST">
-  <input type="text" name="usuario" placeholder="Usuário" required>
-  <input type="password" name="senha" placeholder="Senha" required>
-  <button type="submit">Entrar</button>
-</form>
